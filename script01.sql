1ef2998a851ae7889907c92716e0165336efb9d5
Joabe Oliveira <joabefatec01bd@outlook.com>
2023-09-11 17:42:41 -0300

    terceiro exercicio

--- script01.sql
+++ script01.sql
@@ -1,13 +1,27 @@
 DO $$
 DECLARE
-n1 NUMERIC(5,2);
-limite_inferior NUMERIC(5,2) := 1.0;
-limite_superior NUMERIC(5,2) := 10.0;
+celsius NUMERIC(5,2);
+fahrenheit NUMERIC(5,2);
+limite_inferior NUMERIC(5,2) := 20.0;
+limite_superior NUMERIC(5,2) := 30.0;
 BEGIN
-n1 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::NUMERIC;
-RAISE NOTICE '%',n1;
+celsius := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::NUMERIC;
+fahrenheit := (celsius * 1.8) + 32;
+RAISE NOTICE 'O valor em celsius é %, convertendo para fahrenheit fica %',celsius, fahrenheit;
 END $$;
 
+-- Ex02
+-- DO $$
+-- DECLARE
+-- n1 NUMERIC(5,2);
+-- limite_inferior NUMERIC(5,2) := 1.0;
+-- limite_superior NUMERIC(5,2) := 10.0;
+-- BEGIN
+-- n1 := floor(random() * (limite_superior - limite_inferior + 1) + limite_inferior)::NUMERIC;
+-- RAISE NOTICE '%',n1;
+-- END $$;
+
+-- Ex01
 -- DO $$
 -- DECLARE
 -- n1 INT;
